3ea68c482c8c69f194bb6d0a05b9c9f06c734aad
8282992 <BABABYAGA.A.K82@gmail.com>
2025-10-30 12:21:54 +0330

    new pro

---
+++ product/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from user.models import User
+# Create your models here.
+
+
+# .
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(null=True , blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+# .
+class Course(models.Model):
+    category = models.ForeignKey(to=Category , on_delete=models.PROTECT)
+    title = models.CharField(max_length=20)
+    description = models.TextField(null=True , blank=True)
+    teachers = models.ManyToManyField(User)
+    base_price = models.IntegerField()
+    discount = models.IntegerField()
+    final_price = models.IntegerField()
+    type = models.CharField(
+        max_length=20,
+        choices=[
+        ("online" , "online"),
+        ("offline" ,"offfline")])
+    activate = models.BooleanField()
+    # .
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    class_link = models.URLField(null=True , blank=True)
+    # .
+    duration = models.TimeField()
+
+
+# .
+class Chapter(models.Model):
+    course = models.ForeignKey(Course , on_delete=models.PROTECT)
+    title = models.CharField()
+    number = models.IntegerField()
+
+
+# .
+class Video(models.Model):
+    chapter = models.ForeignKey(Chapter , on_delete=models.PROTECT)
+    title = models.CharField()
+    number = models.IntegerField()
+    file = models.FileField(null=True , blank=True)
+    duration = models.TimeField()
+
+
+# .
+class Attachment(models.Model):
+    chapter = models.ForeignKey(Chapter , on_delete=models.PROTECT)
+    title = models.CharField()
+    number = models.IntegerField()
+    file = models.FileField(null=True , blank=True)
