aa09fbc5829dfcab2e1da5d51d11b9197a414d94
8282992 <BABABYAGA.A.K82@gmail.com>
2025-11-02 11:22:15 +0330

    admin

--- product/admin.py
+++ product/admin.py
@@ -3,6 +3,7 @@ here we are Personalizing admin panel based on models for the admin
 """
 from django.contrib import admin
 from product.models import Category , Course, Chapter, Video, Attachment
+from django.contrib.admin import RelatedOnlyFieldListFilter
 # Register your models here.
 
 
@@ -12,29 +13,33 @@ class Category_Admin(admin.ModelAdmin):
     list_filter = ['name']
 
 class Course_Admin(admin.ModelAdmin):
-    list_display = ['category__name','title','show_teachers','base_price','type','start_date','end_date','duration_time']
-    search_fields = ['category__name','title','description','type']
-    list_filter = ['category__name','title','description','base_price','type']
-    #?
+    list_display = ['category__name','title','show_teachers','final_price','type','activate','start_date','end_date','duration_time']
+    search_fields = ['category__name','title','description','type','activate','start_date']
+    list_filter = [('category__name', RelatedOnlyFieldListFilter),'title','description','final_price','type', 'activate','start_date']
+
     def show_teachers(self, obj):
         return ", ".join([teacher.username for teacher in obj.teachers.all()])
     show_teachers.short_description = "teachers"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('teachers')
 
 
 class Chapter_Admin(admin.ModelAdmin):
     list_display = ['course__title','title','number']
     search_fields = ['course__title','title','number']
-    list_filter = ['course__title','title','number']
+    list_filter = [('course__title', RelatedOnlyFieldListFilter),'title','number']
 
 class Video_Admin(admin.ModelAdmin):
     list_display = ['chapter__title','title','number']
     search_fields = ['chapter__title','title','number']
-    list_filter = ['chapter__title','title','number']
+    list_filter = [('chapter__title', RelatedOnlyFieldListFilter),'title','number']
 
 class Attachment_Admin(admin.ModelAdmin):
     list_display = ['chapter__title','title','number']
     search_fields = ['chapter__title','title','number']
-    list_filter = ['chapter__title','title','number']
+    list_filter = [('chapter__title', RelatedOnlyFieldListFilter),'title','number']
 
 
 admin.site.register(Category,Category_Admin)
